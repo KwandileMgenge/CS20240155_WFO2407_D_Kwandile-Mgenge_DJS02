6e7ea4a5d1c56bc63ab7d571ffe5f6b2031af6a5
Kwandile Mgenge <kwandilen.mgenge@gmail.com>
2024-10-27 15:08:21 +0200

    added validation for empty inputs in a trycatch block

--- scripts.js
+++ scripts.js
@@ -5,6 +5,19 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+
+  try {
+    if (!dividend || !divider) {
+      result.innerText = "Division not performed. Both values are required in inputs. Try again";
+      return;
+    }
+
+    result.innerText = Math.floor(dividend / divider); // Rounds down the result
+
+  } catch (error) {
+    // Critical error handling: replace screen content
+    console.error(error);
+    document.body.innerHTML = "<h1>Something critical went wrong. Please reload the page</h1>";
+  }
   
-  result.innerText = Math.floor(dividend / divider); // Rounds down the result
 });
